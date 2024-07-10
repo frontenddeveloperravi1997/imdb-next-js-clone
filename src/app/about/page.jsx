daf98f95c3d74bcbb128f263e0750cf3bfbad4d1
Sharma, Ravi <Ravi.Sharma.SC@nttdata.com>
2024-07-10 22:44:00 +0530

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 gap-4 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium convallis lectus, vel tincidunt magna dapibus et. Vestibulum non pulvinar nibh. Nulla auctor molestie condimentum. Pellentesque non massa dignissim, porta turpis eget, tempor mi. Maecenas pharetra erat quis magna mollis ullamcorper in sit amet massa. Curabitur cursus ornare efficitur. Ut dictum venenatis ornare. Pellentesque non dolor convallis, tempor quam ut, mattis magna. Nam lobortis ullamcorper urna vel ultricies. Fusce orci purus, viverra sit amet ex vitae, feugiat hendrerit leo. Donec vitae justo sed dolor consequat egestas vel quis erat. Duis tellus magna, mollis in maximus id, volutpat eu nunc.
+        </p>
+        <p>
+        Duis elit tellus, dictum vitae rutrum quis, congue sed mauris. Aenean varius felis vel justo blandit posuere. Cras sem turpis, vulputate vel pretium vitae, tristique eu nunc. Sed ultrices efficitur lectus a aliquet. Sed pretium porta urna a convallis. Praesent faucibus lacinia risus, eget dictum eros iaculis ac. Pellentesque et molestie dui. Donec pharetra dapibus dui ac porttitor. Phasellus sed vestibulum dui. Morbi consequat porta arcu non maximus.
+        </p>
+        <p>
+        Donec pellentesque purus et magna volutpat, vel convallis nibh sodales. Aenean arcu quam, lobortis ut dignissim cursus, maximus eget nisl. Ut vel ipsum nunc. Curabitur id condimentum libero, eu ultrices sem. Integer sit amet magna faucibus, volutpat turpis eu, condimentum ante. Curabitur commodo pretium nunc, sit amet facilisis lacus interdum sed. Nam cursus consequat mattis. Phasellus nulla nulla, bibendum eget sapien suscipit, tempus iaculis odio. Aliquam laoreet elit eleifend commodo pretium. Nunc in cursus ex. Proin laoreet venenatis mattis. Ut non malesuada tellus. Pellentesque tincidunt, neque sit amet viverra volutpat, sem est ornare magna, at tempor elit lectus non libero.
+        </p>
+    </div>
+  )
+}
